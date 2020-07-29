32d6a8be838fa3681c50d6433c6f14f7209b2e99
mateó  Gómez Montoya <mateo-macbook@MacBook-Pro-de-Carlos.local>
2020-07-29 18:56:07 -0500

    Validaciones

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,40 +1,106 @@
 const {response} = require('express')
+const bcrypt =require('bcryptjs')
 const User=require('../models/User')
 
 
-const loginUser= (req,res=response)=>{
+const loginUser= async(req,res=response)=>{
 
     const {nameUser,password}=req.body
 
-    res.json({
-        ok:true,
-        msg:'login',
-        nameUser,
-        password
-   })
-}
-const createUser= async(req,res=response)=>{
-
-    //const {nameUser,email,password}=req.body
-
     try {
 
-        const user = new User(req.body)
-        await user.save()
-    
-        res.status(201).json({
-            ok:true,
-            msg:'registered'
-       })
+        const user =await User.findOne({nameUser})
 
+        if(!user){
+            return res.status(400).json({
+                ok:false,
+                msg:'El usuario no se encuentra registrado'
+            })
+        }    
+        
+        //confirmar password
+        const validPassword =bcrypt.compareSync(password,user.password)
+
+        if(!validPassword){
+            res.status(400).json({
+                ok:false,
+                msg:'Contraseña incorrecta'
+            })
+        }
+
+        //generarJWT
+        res.json({
+            ok:true,
+            uid: user.id,
+            name: user.nameUser
+       })
+        
     } catch (error) {
         console.log(error)
+        
         res.status(500).json({
             ok:false,
             msg:'Error interno comuniquese con el administrador 3116537985'
         })
     }
 }
+
+const createUser= async(req,res=response)=>{
+
+    const {nameUser,email,password}=req.body
+
+    try {
+
+        let user = await User.findOne({email})
+
+        if(user){
+            return res.status(400).json({
+                ok:false,
+                msg:'Un usuario ya existe con el correo ingresado'
+            })
+        }
+
+        user =await User.findOne({nameUser})
+
+        if(user){
+            return res.status(400).json({
+                ok:false,
+                msg:'El usuario ya existe agregue algo mas al nombre de usuario ingresado'
+            })
+        }
+
+        user = new User(req.body)
+
+        //encriptar contraseña
+        const salt= bcrypt.genSaltSync()
+        user.password=bcrypt.hashSync(password,salt)
+
+        await user.save()
+
+        //generarJWT
+        res.json({
+            ok:true,
+            uid: user.id,
+            name: user.nameUser
+       })
+    
+        res.status(201).json({
+            ok:true,
+            uid: user.id,
+            name: user.nameUser
+       })
+
+    } catch (error) {
+
+        console.log(error)
+        
+        res.status(500).json({
+            ok:false,
+            msg:'Error interno comuniquese con el administrador 3116537985'
+        })
+    }
+}
+
 const renewToken= (req,res=response)=>{
     res.json({
         ok:true,
